f40faae0e26ebb8ac63e78fded9d338604445218
Sam Potter <31372513+stpotter16@users.noreply.github.com>
2018-11-12 10:13:53 -0600

    Initial commit of __init__.py file

---
+++ __init__.py
@@ -0,0 +1,11 @@
+""" PySplineFit: Parametrization and fitting of unstructured multidimensional data with NURBS and B-Splines in Python.
+
+.. moduleauthor:: Sam POtter <spotter1642@gmail.com>
+
+"""
+
+__author__ = "Sam Potter"
+__version__ = "1.0"
+__license__ = "MIT"
+
+import numpy as np
