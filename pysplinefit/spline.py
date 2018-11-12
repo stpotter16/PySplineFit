d4a922ef56f40a232c9b17e669babd6d92d2eda2
Sam Potter <31372513+stpotter16@users.noreply.github.com>
2018-11-12 11:29:26 -0600

    Initial commit of spline class module

---
+++ pysplinefit/spline.py
@@ -0,0 +1,9 @@
+"""
+.. module:: spline
+    :platform: Unix, Windows
+    :synopsis: Contains high level classes for containing NURBS and B-Spline Curves and surfaces
+
+.. moduleauthor:: Sam Potter <spotter1642@gmail.com>
+"""
+
+from . import np
