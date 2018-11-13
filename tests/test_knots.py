ecfaf00db108c2d28a9246035b53b4ca0f3bece9
Sam Potter <31372513+stpotter16@users.noreply.github.com>
2018-11-13 10:54:58 -0600

    Adding test for knot span finder based on NURBS book Ex 2.3

--- tests/test_knots.py
+++ tests/test_knots.py
@@ -8,8 +8,24 @@
 '''
 
 from .context import pysplinefit
+from pysplinefit import knots
 
 import pytest
 
+
 def test_find_span():
+
+    degree = 2
+    knot_vector = [0, 0, 0, 1, 2, 3, 4, 4, 5, 5, 5]
+    # n = m - p - 1 -> n + 1 = m + 1 - p - 1
+    num_ctrlps = len(knot_vector) - degree - 1
+    knot = 5.0 / 2.0
+
+    interval = knots.find_span(num_ctrlps, degree, knot, knot_vector)
+
+    # NURBS Book Example 2.3
+    correct = 4
+
+    assert interval == correct
+
 
