e67d16fb3a5de652a1014262a14e8ea47a367ae7
Sam Potter <31372513+stpotter16@users.noreply.github.com>
2018-11-13 10:37:45 -0600

    Adding context file to test folder so relative imports work in test modules

---
+++ tests/context.py
@@ -0,0 +1,7 @@
+import os
+import sys
+
+# Add the library top directory to first position in $PATH variable
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+import dic
