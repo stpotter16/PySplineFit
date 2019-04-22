a96ad110c96ad49ea9646296484b89de09eccdbe
Sam Potter <spotter1642@gmail.com>
2019-04-21 21:32:10 -0500

    adding package info to setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+"""
+    Simple setup.py script for pysplinefit package
+"""
+
+from setuptools import setup, about
+
+# Meta-data
+NAME = 'pysplinefit'
+DESCRIPTION = 'Tools for fitting Spline curves and surfaces to unstructured data'
+URL = 'https://github.com/stpotter16/PySplineFit'
+EMAIL = 'spotter1642@gmail.com'
+AUTHOR = 'Sam Potter'
+REQUIRES_PYTHON = '>=3.6.0'
+VERSION = 1.0
+REQUIRED = ['numpy']
+
+# Call setup
+setup(
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    py_modules=['pyslplinefit'],
+    install_requires=REQUIRED,
+    liscense='MIT'
+)
