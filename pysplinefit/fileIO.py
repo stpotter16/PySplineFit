334773b0af418f585d800699463aaced265cdf97
Sam Potter <spotter1642@gmail.com>
2019-04-22 03:13:20 -0500

    adding fileIO methods for surfaces

--- pysplinefit/fileIO.py
+++ pysplinefit/fileIO.py
@@ -7,17 +7,16 @@
 """
 
 from . import np
-from . import spline
 
 
-def write_curve_to_txt(curveinstance, filename):
+def write_curve_to_txt(curve_instance, filename):
     """
     Save Spline Curve parametrization data to text file.
 
     Saves degree, knot vector, and control points.
 
-    :param curveinstance: Curve object to save
-    :type curveinstance: spline.Curve() object
+    :param curve_instance: Curve object to save
+    :type curve_instance: spline.Curve() object
     :param filename: Path (may be relative or absolute) to destination file. Must include extension.
     :type filename: str
     :return: None
@@ -26,34 +25,34 @@ def write_curve_to_txt(curveinstance, filename):
     with open(filename, 'r') as f:
         # Write degree information
         f.write('Degree - p:\n')
-        f.write('{}\n'.format(curveinstance.degree))
+        f.write('{}\n'.format(curve_instance.degree))
 
         # Write knot vector information
         f.write('Number of Knots\n')
-        f.write('{}\n'.format(len(curveinstance.knot_vector)))
+        f.write('{}\n'.format(len(curve_instance.knot_vector)))
 
         f.write('Knot Vector - U\n')
-        for knot in range(curveinstance.knot_vector):
-            f.write('{}\t'.format(curveinstance.knot_vector[knot]))
+        for knot in range(curve_instance.knot_vector):
+            f.write('{}\t'.format(curve_instance.knot_vector[knot]))
         f.write('\n')
 
         # Write control points info
         f.write('Number of Control Points\n')
-        f.write('{}\n'.format(len(curveinstance.control_points)))
+        f.write('{}\n'.format(len(curve_instance.control_points)))
 
         f.write('Control Points\n')
-        for ctrlpt in range(curveinstance.control_points):
-            f.write('{}\t {}\t {}\n'.format(curveinstance.control_points[ctrlpt, 0],
-                                            curveinstance.control_points[ctrlpt, 1],
-                                            curveinstance.control_points[ctrlpt, 2]))
+        for ctrlpt in range(curve_instance.control_points):
+            f.write('{}\t {}\t {}\n'.format(curve_instance.control_points[ctrlpt, 0],
+                                            curve_instance.control_points[ctrlpt, 1],
+                                            curve_instance.control_points[ctrlpt, 2]))
 
 
-def read_curve_from_txt(curve, filename):
+def read_curve_from_txt(curve_instance, filename):
     """
     Read spline curve data from file and modify an instance of the spline.Curve() class
 
-    :param curve: Curve object to define with data in filename
-    :type curve: spline.Curve() object
+    :param curve_instance: Curve object to define with data in filename
+    :type curve_instance: spline.Curve() object
     :param filename: Path (relative or absolute) to file containing curve data. Must include extension.
     :type filename: str
     :return: None
@@ -80,6 +79,106 @@ def read_curve_from_txt(curve, filename):
         control_points = np.array(control_points)
 
         # Setup the curve
-        curve.degree = degree
-        curve.control_points = control_points
-        curve.knot_vector = knot_vector
+        curve_instance.degree = degree
+        curve_instance.control_points = control_points
+        curve_instance.knot_vector = knot_vector
+
+
+def write_surface_to_txt(surface_instance, filename):
+    """
+    Save Spline Surface parametrization data to text file.
+
+    Saves degree, knot vector, and control points.
+
+    :param surface_instance: Surface object to save
+    :type surface_instance: spline.Surface() object
+    :param filename: Path (may be relative or absolute) to destination file. Must include extension.
+    :type filename: str
+    :return: None
+    :rtype: None
+    """
+    with open(filename, 'r') as f:
+        # Write degree information
+        f.write('Degree - p:\n')
+        f.write('{}\n'.format(surface_instance.degree_u))
+
+        f.write('Degree - q:\n')
+        f.write('{}\n'.format(surface_instance.degree_v))
+
+        # Write knot vector information
+        f.write('Number of Knots in U\n')
+        f.write('{}\n'.format(len(surface_instance.knot_vector_u)))
+
+        f.write('Number of Knots in V\n')
+        f.write('{}\n'.format(len(surface_instance.knot_vector_v)))
+
+        f.write('Knot Vector - U\n')
+        for knot in range(surface_instance.knot_vector_u):
+            f.write('{}\t'.format(surface_instance.knot_vector_u[knot]))
+        f.write('\n')
+
+        f.write('Knot Vector - V\n')
+        for knot in range(surface_instance.knot_vector_v):
+            f.write('{}\t'.format(surface_instance.knot_vector_v[knot]))
+        f.write('\n')
+
+        # Write control points info
+        f.write('Number of Control Points in U\n')
+        f.write('{}\n'.format(surface_instance.num_ctrlpts_u))
+
+        f.write('Number of Control Points in V\n')
+        f.write('{}\n'.format(surface_instance.num_ctrlpts_v))
+
+        f.write('Control Points\n')
+        for ctrlpt in range(surface_instance.control_points):
+            f.write('{}\t {}\t {}\n'.format(surface_instance.control_points[ctrlpt, 0],
+                                            surface_instance.control_points[ctrlpt, 1],
+                                            surface_instance.control_points[ctrlpt, 2]))
+
+
+def read_surf_from_txt(surface_instance, filename):
+    """
+    Read spline surface data from file and modify an instance of the spline.Surface() class
+
+    :param surface_instance: Surface object to define with data in filename
+    :type surface_instance: spline.Surface() object
+    :param filename: Path (relative or absolute) to file containing curve data. Must include extension.
+    :type filename: str
+    :return: None
+    :rtype: None
+    """
+
+    with open(filename, 'r') as f:
+        contents = [line.strip('\n') for line in f]
+
+        # Pull degree
+        degree_u = int(contents[1])
+        degree_v = int(contents[3])
+
+        # Get number of knots
+        num_knots_u = int(contents[5])
+        num_knots_v = int(contents[7])
+
+        # Get knot values
+        knot_vector_u = np.array(list(map(float, contents[9].split('\t'))))
+        knot_vector_v = np.array(list(map(float, contents[11].split('\t'))))
+
+        # Get number of control points
+        num_ctrlpts_u = int(contents[13])
+        num_ctrlpts_v = int(contents[15])
+
+        # Get actual control points
+        control_points = [list(map(float, contents[line].split())) for line in range(17, len(contents))]
+        control_points = np.array(control_points)
+
+        # Setup the curve
+        surface_instance.degree_u = degree_u
+        surface_instance.degree_v = degree_v
+
+        surface_instance.num_ctrlpts_u = num_ctrlpts_u
+        surface_instance.num_ctrlpts_v = num_ctrlpts_v
+
+        surface_instance.control_points = control_points
+
+        surface_instance.knot_vector_u = knot_vector_u
+        surface_instance.knot_vector_v = knot_vector_v
