bbb019954e87ed59a0bd330066680c89636a6629
Sam Potter <spotter1642@gmail.com>
2020-03-22 13:56:59 -0500

    Add basic curve reader and writer for text files

--- fit/io/base_io.py
+++ fit/io/base_io.py
@@ -18,10 +18,35 @@ class BaseFileIO(ABC):
         self.file_handle = file_handle
         super(BaseFileIO, self).__init__(**kwargs)
 
+
+class BaseReader(BaseFileIO):
+    """ A base class for file readers
+    """
     @abc.abstractmethod
     def read(self):
+        """ Read spline information from a file
+
+        Returns
+        -------
+        spline : A SplineGeometry instance
+        """
         raise NotImplementedError
+
+
+class BaseWriter(BaseFileIO):
+    """ A base class for file writers
+
+    Attributes
+    ----------
+    spline : A SplineGeometry instance
+    """
+    def __init__self(spline, *args, **kwargs):
+        self.spline = spline
+        super(BaseReader, self).__init__(*args, **kwargs)
+
 
     @abc.abstractmethod
     def write(self):
+        """ Write spline information to a file
+        """
         raise NotImplementedError
